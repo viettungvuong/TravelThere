da7510758d97b863f6680909bb5bcba756a530ec
viettungvuong <viettungvuong@gmail.com>
2023-08-21 21:07:03 +0700

    Thêm các mục

--- app/src/main/java/com/tung/travelthere/PlaceView.kt
+++ app/src/main/java/com/tung/travelthere/PlaceView.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,7 +25,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
@@ -52,11 +58,11 @@ class PlaceView: ComponentActivity(){
                         .heightIn(max = 300.dp)
                         .fillMaxWidth()
                         .constrainAs(image) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        width = Dimension.fillToConstraints
-                    }
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            width = Dimension.fillToConstraints
+                        }
                 ) {
                     Image(
                         painter = painterResource(id = id!!),
@@ -70,21 +76,40 @@ class PlaceView: ComponentActivity(){
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(Color.White, shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
+                        )
                         .constrainAs(detail) {
                             top.linkTo(image.bottom, margin = 200.dp)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
-                            bottom.linkTo(parent.bottom)}
+                            bottom.linkTo(parent.bottom)
+                        }
                 ) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(16.dp)
                     ) {
-                        // Add your information components here
-                        Text(text = "Lil Wayne")
-                        // Other information components
+                        Text(text = location.getName(), fontWeight = FontWeight.Bold, fontSize = 30.sp)
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ){
+                            Icon(
+                                imageVector = Icons.Default.Place,
+                                contentDescription = "Address",
+                                tint = Color.Black
+                            )
+
+                            Spacer(modifier = Modifier.width(20.dp))
+                            
+                            Text(text = "18 đường số 7")
+                        }
+
                     }
                 }
 
