fdc052ea5bb9b5c3c3f0f25d49b3f8ba752eead9
viettungvuong <viettungvuong@gmail.com>
2023-08-21 20:34:10 +0700

    Trang trí placeview

--- app/src/main/java/com/tung/travelthere/PlaceView.kt
+++ app/src/main/java/com/tung/travelthere/PlaceView.kt
@@ -3,11 +3,15 @@ package com.tung.travelthere
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +22,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
 import com.tung.travelthere.objects.Location
 
@@ -38,13 +44,49 @@ class PlaceView: ComponentActivity(){
         val id = location.getDrawableName(LocalContext.current)
 
         MaterialTheme{
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Image(painter = painterResource(id = id!!), contentDescription = null, modifier =
-                Modifier.height(400.dp), contentScale = ContentScale.Crop,)
+            ConstraintLayout() {
+                val (image, detail) = createRefs()
+
+                Box(
+                    modifier = Modifier
+                        .heightIn(max = 300.dp)
+                        .fillMaxWidth()
+                        .constrainAs(image) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        width = Dimension.fillToConstraints
+                    }
+                ) {
+                    Image(
+                        painter = painterResource(id = id!!),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.White, shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                        .constrainAs(detail) {
+                            top.linkTo(image.bottom, margin = 200.dp)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)}
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        // Add your information components here
+                        Text(text = "Lil Wayne")
+                        // Other information components
+                    }
+                }
 
             }
         }
