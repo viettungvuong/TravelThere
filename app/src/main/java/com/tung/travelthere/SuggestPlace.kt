b116aa7092136274530694caa9a74abf567d7006
viettungvuong <viettungvuong@gmail.com>
2023-08-24 15:55:58 +0700

    State hoist

--- app/src/main/java/com/tung/travelthere/SuggestPlace.kt
+++ app/src/main/java/com/tung/travelthere/SuggestPlace.kt
@@ -13,9 +13,6 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
@@ -28,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tung.travelthere.controller.AppController
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -66,8 +64,10 @@ class SuggestPlace : ComponentActivity() {
     @Composable
     fun suggestPlace(context: Context) {
         var searchPlace by remember { mutableStateOf("") }
-        var chosenPlaceAddress by remember { mutableStateOf("") }
+        var chosenPlaceAddress = remember { mutableStateOf("") }
         var chosenPlaceName by remember { mutableStateOf("") }
+
+        var listState = rememberLazyListState()
 
 
         LaunchedEffect(AppController.placeViewModel.currentName) {
@@ -106,41 +106,7 @@ class SuggestPlace : ComponentActivity() {
                     )
                 }
 
-                LazyColumn {
-                    items(AppController.placeViewModel.placeSuggestions) {
-                        Box(
-                            modifier = Modifier
-                                .padding(10.dp)
-                        ) {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)
-                                    .clickable(onClick = {
-                                        chosenPlaceAddress = it.address
-                                        AppController.placeViewModel.getName(it)
-                                        AppController.placeViewModel.placeSuggestions.clear()
-                                    }
-
-                                    )
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Place,
-                                    contentDescription = "Address",
-                                    tint = Color.Black
-                                )
-
-                                Spacer(modifier = Modifier.width(20.dp))
-
-                                Text(text = it.address)
-
-                            }
-
-
-                        }
-
-                    }
-                }
+                placeSuggestions(chosenPlaceAddress = chosenPlaceAddress, listState)
 
                 Box(
                     modifier = Modifier
@@ -197,6 +163,45 @@ class SuggestPlace : ComponentActivity() {
                     }
                 }
 
+
+            }
+        }
+    }
+
+    @Composable
+    fun placeSuggestions(chosenPlaceAddress: MutableState<String>, listState: LazyListState = rememberLazyListState()){
+        LazyColumn(state = listState) {
+            items(AppController.placeViewModel.placeSuggestions) {
+                Box(
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .clickable(onClick = {
+                                chosenPlaceAddress.value = it.address
+                                AppController.placeViewModel.getName(it)
+                                AppController.placeViewModel.placeSuggestions.clear()
+                            }
+
+                            )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Place,
+                            contentDescription = "Address",
+                            tint = Color.Black
+                        )
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Text(text = it.address)
+
+                    }
+
+
+                }
 
             }
         }
