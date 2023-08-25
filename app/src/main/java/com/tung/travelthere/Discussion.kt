574b206f4aaab686570ba7b806f8499f964fd48b
viettungvuong <viettungvuong@gmail.com>
2023-08-25 23:06:40 +0700

    Thêm composable function

--- app/src/main/java/com/tung/travelthere/Discussion.kt
+++ app/src/main/java/com/tung/travelthere/Discussion.kt
@@ -1,8 +1,28 @@
 package com.tung.travelthere
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
 class Discussion {
 }
 
 class DiscussionViewModel{
+
+}
+
+@Composable
+fun discussionUI{
+
+}
+
+@Composable
+fun discussionList{
+    LazyColumn(content = ){
+
+    }
+}
+
+@Composable
+fun discussionView{
 
 }
