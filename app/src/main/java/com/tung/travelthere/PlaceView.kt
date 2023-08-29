b9c245a5789539d2fc6024d9d8963bb3fdc5147f
viettungvuong <viettungvuong@gmail.com>
2023-08-29 15:52:44 +0700

    Thêm back button

--- app/src/main/java/com/tung/travelthere/PlaceView.kt
+++ app/src/main/java/com/tung/travelthere/PlaceView.kt
@@ -6,8 +6,10 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Top
 import androidx.compose.foundation.lazy.LazyColumn
 
 import androidx.compose.foundation.lazy.LazyRow
@@ -23,9 +25,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,11 +33,13 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -52,6 +54,7 @@ import com.tung.travelthere.objects.City
 import com.tung.travelthere.objects.PlaceLocation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.util.Stack
 
 class PlaceView : ComponentActivity() {
     lateinit var location: PlaceLocation
@@ -78,24 +81,7 @@ class PlaceView : ComponentActivity() {
 
         MaterialTheme {
             ConstraintLayout {
-                val (image, detail) = createRefs()
-
-//                Box(
-//                    modifier = Modifier
-//                        .constrainAs(image) {
-//                            top.linkTo(parent.top)
-//                            start.linkTo(parent.start)
-//                        }
-//                ){
-//                    FloatingActionButton(
-//                        onClick = { finish() },
-//                        backgroundColor = Color.White,
-//                        content = {
-//                            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back", tint = Color.Red)
-//                        }
-//                    )
-//                }
-
+                val (image, button, detail) = createRefs()
 
                 Box(
                     modifier = Modifier
@@ -113,6 +99,21 @@ class PlaceView : ComponentActivity() {
                         contentDescription = null,
                         contentScale = ContentScale.Crop,
                         modifier = Modifier.fillMaxSize()
+                    )
+                }
+
+                Box(
+                    modifier = Modifier.size(32.dp).padding(20.dp).constrainAs(button){
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                ) {
+                    FloatingActionButton(
+                        onClick = { finish() },
+                        backgroundColor = Color.White,
+                        content = {
+                            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back", tint = Color.Red)
+                        }
                     )
                 }
 
