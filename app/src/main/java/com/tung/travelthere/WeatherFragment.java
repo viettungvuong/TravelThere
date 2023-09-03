f077f49366aa006955c70f82587ab80c047c5ede
NhutThi2404 <nhutth2404@gmail.com>
2023-09-04 00:45:42 +0700

    add the fragment check the weather

--- app/src/main/java/com/tung/travelthere/WeatherFragment.java
+++ app/src/main/java/com/tung/travelthere/WeatherFragment.java
@@ -1,22 +1,50 @@
 package com.tung.travelthere;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class WeatherFragment extends Fragment {
 
     private TextView textCityName, textTemperature, textCondition;
     private TextInputEditText editTextCityName;
     private ImageView imgCondition, imgSearch;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
+    private String cityName;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,7 +55,94 @@ public class WeatherFragment extends Fragment {
         imgCondition = getView().findViewById(R.id.image_weather_condition);
         imgSearch = getView().findViewById(R.id.image_weather_search);
 
+        locationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        cityName = getCityName(location.getLongitude(), location.getLatitude());
+        getWeatherInfo(cityName);
+
+        imgSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = editTextCityName.getText().toString();
+                if (city.isEmpty())
+                {
+                    Toast.makeText(getContext(), "Please enter the city name", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    textCityName.setText(cityName);
+                    getWeatherInfo(city);
+                }
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+
+    private String getCityName(double longitude, double latitude){
+        String cityName = "Not found";
+        Geocoder gcd = new Geocoder(getActivity().getBaseContext(), Locale.getDefault());
+        try{
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 10);
+            for (Address adr : addresses)
+            {
+                if (adr != null)
+                {
+                    String city = adr.getLocality();
+                    if (city != null && !city.equals(""))
+                    {
+                        cityName = city;
+                    }
+                    else
+                    {
+                        Log.d("TAG", "City not found");
+                        Toast.makeText(this.getContext(), "City not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return cityName;
+
+    }
+    private void getWeatherInfo(String cityName)
+    {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=50224d22b9804f92a1b94202230309&q="+ cityName+"&days=1&aqi=no&alerts=no";
+        textCityName.setText(cityName);
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String temperature = response.getJSONObject("current").getString("temp_c");
+                    textTemperature.setText(temperature + "°C");
+                    int isDay = response.getJSONObject("current").getInt("is_day");
+                    String condition = response.getJSONObject("current").getJSONObject("condition").getString("text");
+                    String conditionImg = response.getJSONObject("current").getJSONObject("condition").getString("icon");
+                    Picasso.get().load("http:".concat(conditionImg)).into(imgCondition);
+                    textCondition.setText(condition);
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Please enter a valid city name", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
